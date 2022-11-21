50f228b378cb7ffe6b61dcf60b505d1fe004d79a
Swetha Santhosh <swethasanthosh1004@gmail.com>
2022-11-21 12:42:48 +0530

    new

---
+++ CanteenManagementSystem/Models/Admin.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CanteenManagementSystem.Models
+{
+    [Table]
+    public class Admin
+    {
+        [Required(ErrorMessage = "username is Required")]
+        public string UserName { get; set; }
+        [Required(ErrorMessage = "Password is Required")]
+
+        public string Password { get; set; }
+    }
+}
